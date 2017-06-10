a445d796b82f2adb71d35d56ce74a385623c29c1
Svetoslav Stankov <sv.stankoff@gmail.com>
2017-06-10 20:29:40 +0300

    MethodsAndDebuggingLab

---
+++ MethodsAndDebugging/04. FilledSquare/FilledSquare.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.FilledSquare
+{
+    class FilledSquare
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            TopRow(n);
+            MiddleRow(n);
+            BottomRow(n);
+        }
+
+        static void TopRow(int n)
+        {
+            Console.WriteLine(new string('-', 2 * n));
+        }
+
+        static void MiddleRow(int n)
+        {
+            Console.Write("-");
+            for (int i = 1; i < n; i++)
+            {
+                Console.Write("\\/");
+            }
+            
+            Console.WriteLine("-");
+
+            Console.Write("-");
+            for (int i = 1; i < n; i++)
+            {
+                Console.Write("\\/");
+            }
+
+            Console.WriteLine("-");
+        }
+        
+        static void BottomRow(int n)
+        {
+            Console.WriteLine(new string('-', 2 * n));
+        }
+    }
+}
