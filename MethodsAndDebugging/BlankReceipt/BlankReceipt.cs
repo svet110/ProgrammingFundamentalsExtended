bd15d6e5a19ef4efeb8f5d7a7a76fc3c0d337588
Svetoslav Stankov <sv.stankoff@gmail.com>
2017-06-10 17:41:44 +0300

    Rename

---
+++ MethodsAndDebugging/BlankReceipt/BlankReceipt.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAndDebugging
+{
+    class BlankReceipt
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
