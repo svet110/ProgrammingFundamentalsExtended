558a8d915f936257f8e9c89b156f6a822379464e
Svetoslav Stankov <sv.stankoff@gmail.com>
2017-06-11 11:26:52 +0300

    MethodsAndDebuggingLab

---
+++ MethodsAndDebugging/GreaterOfTwoValues/GreaterOfTwoValues.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreaterOfTwoValues
+{
+    class GreaterOfTwoValues
+    {
+        static void Main()
+        {
+            string type = Console.ReadLine();
+            if (type == "string")
+            {
+                string a = Console.ReadLine();
+                string b = Console.ReadLine();
+                Console.WriteLine(GetMax(a, b));
+            }
+            else if (type == "int")
+            {
+                int num1 = int.Parse(Console.ReadLine());
+                int num2 = int.Parse(Console.ReadLine());
+                Console.WriteLine(GetMax(num1, num2));
+            }
+            else if (type == "char")
+            {
+                char char1 = char.Parse(Console.ReadLine());
+                char char2 = char.Parse(Console.ReadLine());
+                Console.WriteLine(GetMax(char1, char2));
+            }
+
+
+        }
+
+        static string GetMax(string a, string b)
+        {
+            if (a.CompareTo(b) >= 0)
+            {
+                return a;
+            }
+            else
+            {
+                return b;
+            }
+        }
+
+        static int GetMax(int a, int b)
+        {
+            if (a > b)
+            {
+                return a;
+            }
+            else 
+            {
+                return b;
+            }
+        }
+
+        static char GetMax(char a, char b)
+        {
+            if (a > b)
+            {
+                return a;
+            }
+            else
+            {
+                return b;
+            }
+        }
+    }
+}
---
+++ MethodsAndDebugging/MathPower/MathPower.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathPower
+{
+    class MathPower
+    {
+        static void Main()
+        {
+            double a = double.Parse(Console.ReadLine());
+            double b = int.Parse(Console.ReadLine());
+            Console.WriteLine(RaiseToPower(a, b));
+        }
+
+        static double RaiseToPower(double num, double power)
+        {
+            double result = Math.Pow(num, power);
+            return result;
+        }
+    }
+}
