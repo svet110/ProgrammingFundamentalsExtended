3ad5d9fb330466932a614f1cb4c334b139f4ab14
Svetoslav Stankov <sv.stankoff@gmail.com>
2017-07-04 20:05:14 +0300

    DictionariesLab

---
+++ DictionariesLab/CountRealNumbers/CountRealNumbers.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DictionariesLab
+{
+    class CountRealNumbers
+    {
+        static void Main()
+        {
+            var numbers = new SortedDictionary<double, int>();
+            double[] input = Console.ReadLine().Split(' ').Select(double.Parse).ToArray();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (!numbers.ContainsKey(input[i]))
+                {
+                    numbers.Add(input[i], 0);
+                }
+
+                numbers[input[i]]++;
+            }
+
+            foreach (var number in numbers.Keys)
+            {
+                Console.WriteLine("{0} -> {1} times", number, numbers[number]);
+            }
+        }
+    }
+}
---
+++ DictionariesLab/OddOccurences/OddOccurences.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionariesLab
+{
+    class OddOccurences
+    {
+        static void Main()
+        {
+            var words = new Dictionary<string, int>();
+            string[] input = Console.ReadLine().ToLower().Split();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (!words.ContainsKey(input[i]))
+                {
+                    words.Add(input[i], 0);
+                }
+
+                words[input[i]]++;
+            }
+
+            var result = new List<string>();
+
+            foreach (var word in words)
+            {
+                if (word.Value % 2 == 1)
+                {
+                    result.Add(word.Key);
+                }
+            }
+
+            Console.WriteLine(string.Join(", ", result));
+        }
+    }
+}
